81f696e1241bdfbf9890d4eb7c87d46d685e24f2
Vasiliy Novikov <vanoviko@microsoft.com>
2021-07-30 14:43:48 +0000

    Benchmark refactored

--- HashSortedDictionary.Benchmarks/QueueBenchmarks.cs
+++ HashSortedDictionary.Benchmarks/QueueBenchmarks.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using BenchmarkDotNet.Attributes;
 
 namespace HashSortedDictionary.Benchmarks
@@ -6,26 +7,29 @@ namespace HashSortedDictionary.Benchmarks
     [MemoryDiagnoser]
     public class QueueBenchmarks
     {
-        //public static readonly IEnumerable<int> BucketSizes = new[] {4, 16, 64, 256}; 
-        
+        public static IEnumerable<object[]> Queues() => new[]
+        {
+            new object[] {BenchmarkingQueueType.Sorted, null},
+            new object[] {BenchmarkingQueueType.HashSorted, 2},
+            new object[] {BenchmarkingQueueType.HashSorted, 4},
+            new object[] {BenchmarkingQueueType.HashSorted, 6},
+            new object[] {BenchmarkingQueueType.HashSorted, 8}
+        };
+
         [Params(1000, 100000)]
         public int KeyRange { get; set; }
-        
+
         [Params(100, 10000)]
         public int Size { get; set; }
 
         [Benchmark]
-        [Arguments(BenchmarkingQueueType.Sorted, 0)]
-        [Arguments(BenchmarkingQueueType.HashSorted, 2)]
-        [Arguments(BenchmarkingQueueType.HashSorted, 4)]
-        [Arguments(BenchmarkingQueueType.HashSorted, 6)]
-        [Arguments(BenchmarkingQueueType.HashSorted, 8)]
-        public void Enqueue_Dequeue_Benchmark(BenchmarkingQueueType type, int bucketSizeBits)
+        [ArgumentsSource(nameof(Queues))]
+        public void Enqueue_Dequeue_Benchmark(BenchmarkingQueueType type, int? bucketSizeBits)
         {
             IBenchmarkingQueue<int> queue = type switch
             {
                 BenchmarkingQueueType.Sorted => new SortedBenchmarkingQueue<int>(),
-                BenchmarkingQueueType.HashSorted => new HashSortedBenchmarkingQueue<int>(k => (uint)k, (byte)bucketSizeBits),
+                BenchmarkingQueueType.HashSorted => new HashSortedBenchmarkingQueue<int>(k => (uint)k, (byte)bucketSizeBits!.Value),
                 _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
             };
 
@@ -33,7 +37,7 @@ namespace HashSortedDictionary.Benchmarks
             for (var i = 0; i < Size; ++i) 
                 queue.Enqueue(rnd.Next(KeyRange));
 
-            for (var i = 0; i < Size * 100; ++i)
+            for (var i = 0; i < Size * 20; ++i)
             {
                 queue.Dequeue();
                 queue.Enqueue(rnd.Next(KeyRange));
